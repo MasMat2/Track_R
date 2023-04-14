988dafb90b4badd37faf7d15d3211f73d684357c
Osvaldo Ortega <osvaldo.ortega01@gmail.com>
2023-04-14 12:24:04 -0600

    Retroalimentacion QA - home-slide y login
    Aplicar ancho máximo botones login y registrar
    Arreglar bug visual input activo y error
    Arreglo botón entrar en login

--- Frontend/trackr-paciente/src/app/pages/login/login.page.ts
+++ Frontend/trackr-paciente/src/app/pages/login/login.page.ts
@@ -3,7 +3,8 @@ import { LoginRequest } from 'src/app/models/seguridad/login-request';
 import { LoginService } from './../../services/seguridad/login.service';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import * as Utileria from '@shared/utileria';
 import { IonicModule } from '@ionic/angular';
 import { Router, RouterLink } from '@angular/router';
 import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
@@ -23,6 +24,8 @@ import { GeneralConstant } from '@shared/general-constant';
 })
 export class LoginPage implements OnInit {
   loginRequest: LoginRequest = new LoginRequest();
+  loginResponse: LoginResponse = new LoginResponse();
+  btnSubmit: boolean = false;
   constructor(
     private loginService: LoginService,
     private router: Router
@@ -32,14 +35,42 @@ export class LoginPage implements OnInit {
 
   }
 
+  /**
+   * Valida que los campos del formulario sean requeridos y que el correo y la contraseña no estén vacíos.
+   * Si es exitoso, llama al método autenticar.
+   * @param formulario Formulario a validar.
+   * @returns
+   */
+  public async enviarFormulario(formulario: NgForm){
+    this.btnSubmit = true;
+    if(formulario.invalid){
+      Utileria.validarCamposRequeridos(formulario);
+      this.btnSubmit = false;
+      return;
+    }
+
+    if(!this.loginRequest.correo || !this.loginRequest.contrasena){
+      this.btnSubmit = false;
+      return;
+    }
+
+    this.autenticar();
+    this.btnSubmit = false;
+  }
+
+  /**
+   * Realiza la autenticación del usuario utilizando el servicio de inicio de sesión y redirige a la página de pacientes.
+   * Llama al método Authenticate de LoginService. Si es exitoso, guarda el LoginResponse Token en el localStorage y redirige a la página de pacientes.
+   */
   public async autenticar() {
     await lastValueFrom(this.loginService.authenticate(this.loginRequest))
-          .then(loginResponse => {
+          .then((loginResponse: LoginResponse) => {
             localStorage.setItem(GeneralConstant.TOKEN_KEY, loginResponse.token);
             this.router.navigate(['/paciente']);
           })
           .catch(error => {
             localStorage.removeItem(GeneralConstant.TOKEN_KEY);
+            this.btnSubmit = false;
           });
   }
 
--- Frontend/trackr-paciente/src/app/services/seguridad/login.service.ts
+++ Frontend/trackr-paciente/src/app/services/seguridad/login.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from 'src/app/models/seguridad/login-request';
+import { LoginResponse } from 'src/app/models/seguridad/login-response';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class LoginService {
 
   constructor(public http: HttpClient) { }
 
-  public authenticate(loginRequest: LoginRequest): Observable<any> {
-    return this.http.post<any>(this.dataUrl + 'authenticate/', loginRequest);
+  public authenticate(loginRequest: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.dataUrl + 'authenticate/', loginRequest);
   }
 }
