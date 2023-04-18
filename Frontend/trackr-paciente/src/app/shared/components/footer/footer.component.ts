5010b9ecb3dae32085787151346e99bcfc4e0b7c
Osvaldo Ortega <osvaldo.ortega01@gmail.com>
2023-04-18 10:44:48 -0600

    Agregar Action Sheet para cerrar sesión

--- Frontend/trackr-paciente/src/app/shared/components/footer/footer.component.ts
+++ Frontend/trackr-paciente/src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { GeneralConstant } from '@shared/general-constant';
 
 @Component({
   selector: 'app-footer',
@@ -7,15 +8,58 @@ import { Router } from '@angular/router';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Cerrar Sesión',
+      role: 'destructive',
+      icon:'log-out',
+      cssClass: 'rojo',
+      data: {
+        action: 'logout'
+      }
+    },
+    {
+      text: 'Ver Perfil',
+      icon:'person',
+      data: {
+        action: 'seeProfile'
+      }
+    },
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      icon:'close',
+      data: {
+        action: 'cancel'
+      }
+    }
+  ];
   constructor(
-    private router: Router) 
+    private router: Router)
   {}
 
   public ngOnInit(): void {}
 
   public cerrarSesion(): void {
+    localStorage.removeItem(GeneralConstant.TOKEN_KEY);
     this.router.navigate(['/login']);
   }
 
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
+
+  setResult(event: any) {
+    switch ((<CustomEvent>event).detail.data?.action) {
+      case 'logout':
+        this.cerrarSesion();
+        break;
+      case 'seeProfile':
+        this.router.navigate(['/perfil']); //TODO: Reemplazar la ruta por la ruta del perfil
+        break;
+      default:
+        break;
+    }
+  }
 }
