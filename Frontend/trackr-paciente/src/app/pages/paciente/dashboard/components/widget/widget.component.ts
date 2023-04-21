88d713db28ebd45b0b3d1e68a47370e8afa4acc2
Ángel Tejeda <angel.tejedat@gmail.com>
2023-04-21 12:56:42 -0600

    Agregar widget base

---
+++ Frontend/trackr-paciente/src/app/pages/paciente/dashboard/components/widget/widget.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+  ]
+})
+export class WidgetComponent  implements OnInit {
+
+  @Input() public title: string;
+  @Input() public iconClass?: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
