94127d7d9bfc716ec112bb1ffba44a9db816071c
Ángel Tejeda <angel.tejedat@gmail.com>
2023-04-25 12:07:04 -0600

    Cargar widgets desde la Base de Datos

--- Frontend/trackr-paciente/src/app/pages/paciente/dashboard/dashboard.page.ts
+++ Frontend/trackr-paciente/src/app/pages/paciente/dashboard/dashboard.page.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { take } from 'rxjs';
+import { UsuarioWidgetService } from 'src/app/services/dashboard/usuario-widget.service';
 import { WidgetContainerComponent } from './components/widget-container/widget-container.component';
 import { WidgetType } from './interfaces/widgets';
 
@@ -15,21 +17,26 @@ import { WidgetType } from './interfaces/widgets';
     CommonModule,
     FormsModule,
     WidgetContainerComponent,
+  ],
+  providers: [
+    UsuarioWidgetService,
   ]
 })
 export class DashboardPage implements OnInit {
 
   constructor(
+    private usuarioWidgetService: UsuarioWidgetService,
   ) { }
 
-  protected widgets: WidgetType[] = [
-    'w-pasos',
-    'w-peso',
-    'w-peso',
-    'w-pasos',
-  ];
+  protected widgets: WidgetType[] = [];
 
   public ngOnInit(): void {
+    this.usuarioWidgetService
+      .consultarPorUsuarioEnSesion()
+      .pipe(take(1))
+      .subscribe(widgets => {
+        this.widgets = widgets;
+      });
   }
 
 }
---
+++ Frontend/trackr-paciente/src/app/services/dashboard/usuario-widget.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { WidgetType, isWidgetType } from 'src/app/pages/paciente/dashboard/interfaces/widgets';
+
+@Injectable()
+export class UsuarioWidgetService {
+  private dataUrl = 'usuarioWidget/';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public consultarPorUsuarioEnSesion(): Observable<WidgetType[]> {
+    return this.http.get<string[]>(this.dataUrl + 'consultarPorUsuarioEnSesion')
+      .pipe(
+        tap(claves => {
+          for (const clave of claves) {
+            if (!isWidgetType(clave)) {
+              throw new Error(`Clave de Widget Inválida: ${clave}`);
+            }
+          }
+        }),
+        map(claves => claves as WidgetType[])
+      );
+  }
+}
