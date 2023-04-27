c58a2aebaa07abb0c0dc087608cf9bb2ebef5dbe
Ángel Tejeda <angel.tejedat@gmail.com>
2023-04-27 15:20:09 -0600

    Agregar configuración de dashboard en módulo paciente

---
+++ Frontend/trackr-paciente/src/app/pages/paciente/configuracion-dashboard/configuracion-dashboard.page.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Widget } from 'src/app/models/dashboard/widget';
+import { UsuarioWidgetService } from 'src/app/services/dashboard/usuario-widget.service';
+import { WidgetService } from 'src/app/services/dashboard/widget.service';
+import { WidgetType } from '../dashboard/interfaces/widgets';
+import { Observable, combineLatestWith, map, tap } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+interface WidgetSeleccionado extends Widget {
+  seleccionado: boolean;
+}
+
+@Component({
+  selector: 'app-configuracion-dashboard',
+  templateUrl: './configuracion-dashboard.page.html',
+  styleUrls: ['./configuracion-dashboard.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule
+  ],
+  providers: [
+    WidgetService,
+    UsuarioWidgetService,
+  ]
+})
+export class ConfiguracionDashboardPage  implements OnInit {
+
+  constructor(
+    private widgetService: WidgetService,
+    private usuarioWidgetService: UsuarioWidgetService,
+  ) { }
+
+  protected widgets: WidgetSeleccionado[] = [];
+
+  ngOnInit() {
+    // TODO: 2023-04-26 -> Take 1
+
+    const widgets$ = this.widgetService.consultar();
+    const usuarioWidgets$ = this.usuarioWidgetService.consultarPorUsuarioEnSesion();
+
+    const widgetsSeleccionados$ = widgets$.pipe(
+      combineLatestWith(usuarioWidgets$),
+      map(([widgets, usuarioWidgets]) => {
+        return widgets.map(widget => {
+          const widgetSeleccionado: WidgetSeleccionado = {
+            ...widget,
+            seleccionado: usuarioWidgets.includes(widget.clave),
+          };
+
+          console.log(widgetSeleccionado);
+
+          return widgetSeleccionado;
+        });
+      })
+    );
+
+    widgetsSeleccionados$.subscribe({
+      next: widgets => {
+        this.widgets = widgets;
+      }
+    });
+  }
+
+  public onChangeCheckbox(event: any) {
+    console.log(event);
+  }
+
+  protected onAceptar(): void {
+    console.log(this.widgets);
+    const seleccionados = this.widgets
+      .filter(widget => widget.seleccionado)
+      .map(widget => widget.clave);
+
+    console.log(seleccionados);
+  }
+}
--- Frontend/trackr-paciente/src/app/pages/paciente/paciente-routes.ts
+++ Frontend/trackr-paciente/src/app/pages/paciente/paciente-routes.ts
@@ -1,6 +1,9 @@
 import { Route } from "@angular/router";
 import { DashboardPage } from "./dashboard/dashboard.page";
+import { ConfiguracionDashboardPage } from "./configuracion-dashboard/configuracion-dashboard.page";
 
 export default [
-    { path: '', component: DashboardPage }
+    { path: '', component: DashboardPage },
+    { path: 'configuracion', component: ConfiguracionDashboardPage }
+
 ] as Route[];
