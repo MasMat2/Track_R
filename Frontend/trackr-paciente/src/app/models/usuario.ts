b0441c000212bd16a202ce37a1be748a92ab60fd
Yael2407 <edson2gest73@gmail.com>
2023-05-05 15:23:15 -0600

    Formulario Registro
    Se realizan ajustes en el formulario para mejorar su funcionamiento

---
+++ Frontend/trackr-paciente/src/app/models/usuario.ts
@@ -0,0 +1,13 @@
+export class Usuario{
+  public idUsuario: number;
+  public nombre: string;
+  public apellidoPaterno: string;
+  public apellidoMaterno: string;
+  public telefonoMovil: number;
+  public correo: string;
+  public contrasena: string;
+  public idPerfil: number;
+  public idTipoUsuario: number;
+  public idCompania: number;
+
+}
--- Frontend/trackr-paciente/src/app/pages/paciente/registro/registro.page.ts
+++ Frontend/trackr-paciente/src/app/pages/paciente/registro/registro.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/seguridad/usuario.service';
+import * as Utileria from "@shared/utileria";
 
 @Component({
   selector: 'app-registro',
@@ -11,10 +14,38 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class RegistroPage implements OnInit {
+  usuario = new Usuario;
+  public disableSubmit: boolean = false;
+  public confirmarContrasena: string = '';
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+  ) { }
 
   ngOnInit() {
   }
+
+  public enviarFormulario(formulario: NgForm): void {
+    this.disableSubmit = true;
+
+    if (!formulario.valid) {
+        Utileria.validarCamposRequeridos(formulario);
+        this.disableSubmit = false;
+        return;
+    }
+
+    this.usuario.idTipoUsuario = 2;
+    this.usuario.idCompania = 1;
+    this.usuario.idPerfil = 1;
+    this.agregar();
+}
+
+protected agregar(): Promise<boolean> {
+    return this.usuarioService
+        .agregar(this.usuario)
+        .toPromise()
+        .then(() => true)
+        .catch(() => false);
+}
 
 }
