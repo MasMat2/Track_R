52d5131706f702349c11da2e682c8f1290082622
Ángel Tejeda <angel.tejedat@gmail.com>
2023-05-09 15:23:32 -0600

    Agregar controller de colonia

---
+++ Backend/TrackrAPI/TrackrAPI/Services/Catalogo/ColoniaService.cs
@@ -0,0 +1,51 @@
+﻿using TrackrAPI.Dtos.Catalogo;
+using TrackrAPI.Models;
+using TrackrAPI.Repositorys.Catalogo;
+using System.Collections.Generic;
+
+namespace TrackrAPI.Services.Catalogo
+{
+    public class ColoniaService
+    {
+        private IColoniaRepository coloniaRepository;
+        private ColoniaValidatorService coloniaValidatorService;
+
+        public ColoniaService(
+            IColoniaRepository coloniaRepository,
+            ColoniaValidatorService coloniaValidatorService
+        )
+        {
+            this.coloniaRepository = coloniaRepository;
+            this.coloniaValidatorService = coloniaValidatorService;
+        }
+
+        public IEnumerable<Colonia> ConsultarPorCodigoParaSelector(string codigoPostal)
+        {
+            return coloniaRepository.ConsultarPorCodigoParaSelector(codigoPostal);
+        }
+
+        public IEnumerable<ColoniaGridDto> ConsultarParaGrid()
+        {
+            return coloniaRepository.ConsultarParaGrid();
+        }
+
+        public void Agregar(Colonia colonia)
+        {
+            this.coloniaValidatorService.ValidarAgregar(colonia);
+            this.coloniaRepository.Agregar(colonia);
+        }
+
+        public void Editar(Colonia colonia)
+        {
+            this.coloniaValidatorService.ValidarEditar(colonia);
+            this.coloniaRepository.Editar(colonia);
+        }
+
+        public void Eliminar(int idColonia)
+        {
+            var colonia = coloniaRepository.Consultar(idColonia);
+            this.coloniaValidatorService.ValidarEliminar(colonia.IdColonia);
+            this.coloniaRepository.Eliminar(colonia);
+        }
+    }
+}
---
+++ Backend/TrackrAPI/TrackrAPI/Services/Catalogo/ColoniaValidatorService.cs
@@ -0,0 +1,73 @@
+﻿using TrackrAPI.Helpers;
+using TrackrAPI.Models;
+using TrackrAPI.Repositorys.Catalogo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackrAPI.Services.Catalogo
+{
+    public class ColoniaValidatorService
+    {
+        private IColoniaRepository coloniaRepository;
+
+        public ColoniaValidatorService(IColoniaRepository coloniaRepository)
+        {
+            this.coloniaRepository = coloniaRepository;
+        }
+
+        private readonly string MensajeCodigoPostalRequerido = "El codigo postal de la colonia es requerido";
+        private readonly string MensajeClaveRequerida = "La clave de la colonia es requerida";
+        private readonly string MensajeNombreRequerido = "El nombre de la colonia es requerido";
+
+        private readonly string MensajeExistencia = "La colonia no existe";
+
+        private readonly string MensajeDuplicadoCodigoPostal = "El código postal ya se encuentra registrado.";
+
+
+        public void ValidarAgregar(Colonia colonia)
+        {
+            ValidarRequerido(colonia);
+            ValidarDuplicado(colonia);
+        }
+        public void ValidarEditar(Colonia colonia)
+        {
+            ValidarRequerido(colonia);
+            ValidarExistencia(colonia.IdColonia);
+            ValidarDuplicado(colonia);
+        }
+
+        public void ValidarEliminar(int idColonia)
+        {
+            ValidarExistencia(idColonia);
+        }
+
+        public void ValidarRequerido(Colonia colonia)
+        {
+            Validator.ValidarRequerido(colonia.Clave, MensajeClaveRequerida);
+            Validator.ValidarRequerido(colonia.CodigoPostal, MensajeCodigoPostalRequerido);
+            Validator.ValidarRequerido(colonia.CodigoPostal, MensajeNombreRequerido);
+        }
+
+        public void ValidarExistencia(int idColonia)
+        {
+            var colonia = coloniaRepository.Consultar(idColonia);
+
+            if (colonia == null)
+            {
+                throw new CdisException(MensajeExistencia);
+            }
+        }
+
+        public void ValidarDuplicado(Colonia colonia)
+        {
+            Colonia duplicadoCodigoPostal = coloniaRepository.ConsultarPorCodigoPostal(colonia.CodigoPostal);
+
+            if (duplicadoCodigoPostal != null && colonia.IdColonia != duplicadoCodigoPostal.IdColonia)
+            {
+                throw new CdisException(MensajeDuplicadoCodigoPostal);
+            }
+        }
+    }
+}
