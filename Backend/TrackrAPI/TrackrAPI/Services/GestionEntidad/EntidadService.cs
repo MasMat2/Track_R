7819282883a0ba59ff9c7ab1773a232ac2f8edd2
Ángel Tejeda <angel.tejedat@gmail.com>
2023-06-12 08:43:06 -0600

    Ajustar actualización de la sección de medidas

--- Backend/TrackrAPI/TrackrAPI/Services/GestionEntidad/EntidadService.cs
+++ Backend/TrackrAPI/TrackrAPI/Services/GestionEntidad/EntidadService.cs
@@ -13,16 +13,22 @@ namespace TrackrAPI.Services.GestionEntidad
         private readonly IEntidadEstructuraRepository entidadEstructuraRepository;
         private readonly EntidadValidatorService entidadValidatorService;
         private readonly EntidadEstructuraService entidadEstructuraService;
+        private readonly SeccionCampoService seccionCampoService;
+        private readonly SeccionService seccionService;
 
         public EntidadService(IEntidadRepository entidadRepository,
             IEntidadEstructuraRepository entidadEstructuraRepository,
             EntidadValidatorService entidadValidatorService,
-            EntidadEstructuraService entidadEstructuraService)
+            EntidadEstructuraService entidadEstructuraService,
+            SeccionCampoService seccionCampoService,
+            SeccionService seccionService)
         {
             this.entidadRepository = entidadRepository;
             this.entidadEstructuraRepository = entidadEstructuraRepository;
             this.entidadValidatorService = entidadValidatorService;
             this.entidadEstructuraService = entidadEstructuraService;
+            this.seccionCampoService = seccionCampoService;
+            this.seccionService = seccionService;
         }
 
         public IEnumerable<EntidadGridDto> ConsultarTodosParaGrid()
@@ -104,8 +110,8 @@ namespace TrackrAPI.Services.GestionEntidad
                 new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted }
             );
 
-            AgregarNuevasMuestras(aeParams);
-            ActualizarPadecimientos(aeParams);
+            ActualizarMuestras(aeParams);
+            // ActualizarPadecimientos(aeParams);
 
             ts.Complete();
         }
@@ -118,49 +124,84 @@ namespace TrackrAPI.Services.GestionEntidad
                 throw new CdisException($"No se encontró la entidad {nombreEntidad} con clave {clave}");
             }
 
-            var arbolPadecimientos = entidadEstructuraService.ConsultarArbol(entidad.IdEntidad);
+            var arbolPadecimientos = entidadEstructuraService.ConsultarParaTabulador(entidad.IdEntidad);
             return arbolPadecimientos.ToList();
         }
 
-        private void AgregarNuevasMuestras(ActualizacionExpedienteParams aeParams)
+        private void ActualizarMuestras(ActualizacionExpedienteParams aeParams)
         {
-            var seccionesPadecimientos = aeParams.ArbolPadecimientos
+            // TODO: 2023-06-07 -> Constante
+            const string claveSeccionMedidas = "SE-010";
+            var seccionMedidas = seccionService.ConsultarPorClave(claveSeccionMedidas);
+
+            if (seccionMedidas == null)
+            {
+                throw new CdisException("No se encontró la sección <strong>Medidas</strong>");
+            }
+
+            var camposMedidas = seccionCampoService.ConsultarPorSeccion(seccionMedidas.IdSeccion);
+
+            var idsSeccionesPadecimientos = aeParams.ArbolPadecimientos
                 .SelectMany(padecimiento => padecimiento.Hijos.Select(h => h.IdSeccion))
                 .Distinct();
 
-            var pestanaMuestras = BuscarPestanaMuestrasEnExpediente(aeParams.ArbolExpediente);
-
-            var nuevasMuestras = seccionesPadecimientos
-                .Except(pestanaMuestras.Hijos.Select(e => e.IdSeccion))
-                .Select(m => new EntidadEstructuraDto
-                {
-                    IdEntidad = pestanaMuestras.IdEntidad,
-                    Nombre = null,
-                    Clave = null,
-                    Tabulacion = null,
-                    IdSeccion = m,
-                    IdEntidadEstructuraPadre = pestanaMuestras.IdEntidadEstructura,
-                })
-                .ToList();
-
-            entidadEstructuraService.Agregar(nuevasMuestras);
-        }
-
-        private static EntidadEstructuraDto BuscarPestanaMuestrasEnExpediente(IEnumerable<EntidadEstructuraDto> arbolExpediente)
-        {
-            const string clavePestanaMuestras = "005";
-
-            var pestanaMuestras = arbolExpediente
-                .FirstOrDefault(e => e.Clave == clavePestanaMuestras);
-
-            if (pestanaMuestras == null)
+            var fila = 0;
+            foreach (var idSeccion in idsSeccionesPadecimientos)
             {
-                throw new CdisException("No se encontró la pestaña de muestras con clave " + clavePestanaMuestras + " en la entidad Expediente Trackr");
-            }
+                var seccionPadecimiento = aeParams.ArbolPadecimientos
+                    .Where(padecimiento => padecimiento.Hijos.Any(h => h.IdSeccion == idSeccion))
+                    .Select(padecimiento => padecimiento.Hijos.Find(h => h.IdSeccion == idSeccion))
+                    .FirstOrDefault()!;
 
-            return pestanaMuestras;
+                foreach (var campo in seccionPadecimiento.Campos)
+                {
+                    var campoMedidas = camposMedidas.FirstOrDefault(c => c.Clave == "ME-" + campo.Clave);
+
+                    if (campoMedidas != null)
+                    {
+                        // Editar
+                        var campoDb = new SeccionCampo
+                        {
+                            IdSeccionCampo = campoMedidas.IdSeccionCampo,
+                            IdSeccion = campoMedidas.IdSeccion,
+                            IdDominio = campoMedidas.IdDominio,
+                            Clave = campoMedidas.Clave,
+                            Descripcion = campo.Descripcion,
+                            Orden = campo.Orden,
+                            Deshabilitado = campo.Deshabilitado,
+                            Requerido = campo.Requerido,
+                            TamanoColumna = campo.TamanoColumna,
+                            Grupo = campoMedidas.Grupo,
+                            Fila = campoMedidas.Fila,
+                        };
+
+                        seccionCampoService.Editar(campoDb);
+                    }
+                    else
+                    {
+                        var nuevoCampo = new SeccionCampo
+                        {
+                            IdSeccion = seccionMedidas.IdSeccion!,
+                            IdDominio = campo.IdDominio,
+                            Clave = "ME-" + campo.Clave,
+                            Descripcion = campo.Descripcion,
+                            Orden = campo.Orden,
+                            Deshabilitado = campo.Deshabilitado,
+                            Requerido = campo.Requerido,
+                            TamanoColumna = campo.TamanoColumna,
+                            Grupo = seccionPadecimiento.Nombre,
+                            Fila = fila,
+                        };
+
+                        seccionCampoService.Agregar(nuevoCampo);
+                    }
+                }
+
+                fila++;
+            }
         }
 
+        // TODO: 2023-06-12 -> Eliminar métodos
         private void ActualizarPadecimientos(ActualizacionExpedienteParams aeParams)
         {
             ActualizarPadecimientosExistentes(aeParams);
