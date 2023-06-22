6d08615b5e5db7179d820e2677f0ae9eb70837e7
Ángel Tejeda <angel.tejedat@gmail.com>
2023-06-22 16:52:18 -0600

    Ajustar formulario de registro

--- Frontend/trackr-paciente/src/app/pages/paciente/registro/registro.page.ts
+++ Frontend/trackr-paciente/src/app/pages/paciente/registro/registro.page.ts
@@ -1,59 +1,53 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { CheckboxCustomEvent, IonicModule } from '@ionic/angular';
+import * as Utileria from '@shared/utileria';
 import { Usuario } from 'src/app/models/usuario';
 import { UsuarioService } from 'src/app/services/seguridad/usuario.service';
-import * as Utileria from "@shared/utileria";
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class RegistroPage implements OnInit {
-  usuario = new Usuario;
-  public disableSubmit: boolean = false;
-  public confirmarContrasena: string = '';
-  checkedbtn : boolean = true
+  protected usuario = new Usuario();
+  protected confirmarContrasena: string = '';
+  protected submitting: boolean = false;
+  protected termsAccepted: boolean = false;
 
-  constructor(
-    private usuarioService: UsuarioService,
-  ) { }
+  constructor(private usuarioService: UsuarioService) {}
 
-  ngOnInit() {
-  }
+  public ngOnInit() {}
 
-  public enviarFormulario(formulario: NgForm): void {
-    this.disableSubmit = true;
+  protected enviarFormulario(formulario: NgForm): void {
+    this.submitting = true;
 
     if (!formulario.valid) {
-        Utileria.validarCamposRequeridos(formulario);
-        this.disableSubmit = false;
-        return;
+      Utileria.validarCamposRequeridos(formulario);
+      this.submitting = false;
+      return;
     }
 
     this.usuario.idTipoUsuario = 2;
     this.usuario.idCompania = 1;
     this.usuario.idPerfil = 1;
     this.agregar();
-}
-
-changeEvent(event: any) {
-  if (event.target.checked) {
-    this.checkedbtn = false
   }
-  this.checkedbtn = true;
-}
 
-protected agregar(): Promise<boolean> {
+  protected changeEvent(event: Event) {
+    const checkboxEvent = event as CheckboxCustomEvent;
+    this.termsAccepted = checkboxEvent.target.checked;
+  }
+
+  protected agregar(): Promise<boolean> {
     return this.usuarioService
-        .agregar(this.usuario)
-        .toPromise()
-        .then(() => true)
-        .catch(() => false);
-}
-
+      .agregar(this.usuario)
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
+  }
 }
