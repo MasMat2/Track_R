3d6b94b5481ed48f4a9d172e4c57bef9e912efb8
Adrian Munoz <amaxmunoz98@gmail.com>
2023-10-10 10:42:57 -0600

    Agregar urlFrontEnd environment.prod

--- Frontend/trackr-paciente/src/environments/environment.prod.ts
+++ Frontend/trackr-paciente/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  urlBackend: 'https://www.atisc.com/trackr_dev/api/'
+  urlBackend: 'https://www.atisc.com/trackr_dev/api/',
+  urlFrontend: 'https://www.atisc.com/trackr_dev/#/',
 };
