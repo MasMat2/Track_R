01e1b7df3ae6ee7b501de25460ed83df73d31e82
Felix Alanis <felixyahveh@gmail.com>
2023-10-19 13:57:33 -0600

    add(web): tab recomendaciones generales

---
+++ Frontend/trackr-administrador/src/app/views/configuracion-general/recomendacion-general/recomendacion-general.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recomendacion-general',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recomendacion-general.component.html',
+  styleUrls: ['./recomendacion-general.component.scss']
+})
+export class RecomendacionGeneralComponent {
+
+}
