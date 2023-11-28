4de73271c0e2c6686a2e522043af7311d3968c4c
Johann <johann_antonio85@hotmail.com>
2023-11-28 16:18:39 -0600

    fix(mobile): problema con muestra de widgets en dashboard

--- Frontend/trackr-paciente/src/app/pages/home/configuracion-dashboard/configuracion-dashboard.page.ts
+++ Frontend/trackr-paciente/src/app/pages/home/configuracion-dashboard/configuracion-dashboard.page.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, AlertController } from '@ionic/angular';
 import { Widget } from 'src/app/models/dashboard/widget';
 import { UsuarioWidgetService } from 'src/app/services/dashboard/usuario-widget.service';
 import { WidgetService } from 'src/app/services/dashboard/widget.service';
 import { Observable, combineLatestWith, map, tap } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../layout/header/header.component';
+import { Router } from '@angular/router';
 
 interface WidgetSeleccionado extends Widget {
   seleccionado: boolean;
@@ -33,9 +34,12 @@ export class ConfiguracionDashboardPage  implements OnInit {
   constructor(
     private widgetService: WidgetService,
     private usuarioWidgetService: UsuarioWidgetService,
+    private alertController: AlertController,
+    private router: Router
   ) { }
 
   protected widgets: WidgetSeleccionado[] = [];
+  protected submiting = false;
 
   ngOnInit() {
     this.consultarWidgets();
@@ -45,7 +49,6 @@ export class ConfiguracionDashboardPage  implements OnInit {
 
     const widgets$ = this.widgetService.consultar();
     const usuarioWidgets$ = this.usuarioWidgetService.consultarPorUsuarioEnSesion();
-
     const widgetsSeleccionados$ = widgets$.pipe(
       combineLatestWith(usuarioWidgets$),
       map(([widgets, usuarioWidgets]) => {
@@ -68,16 +71,34 @@ export class ConfiguracionDashboardPage  implements OnInit {
 
   }
 
-  protected modificarWidgets(widgets: any){
-    this.usuarioWidgetService.modificarPorUsuarioEnSesion(widgets).subscribe({
-      next: () => {}
-    });
-  }
   
   protected onAceptar(): void {
+    this.submiting = true;
     const seleccionados = this.widgets
       .filter(widget => widget.seleccionado)
       .map(widget => widget.clave);
-    this.modificarWidgets(seleccionados);
+
+      this.usuarioWidgetService.modificarPorUsuarioEnSesion(seleccionados).subscribe({
+        next: () => {
+          this.presentAlert();
+          this.submiting = false;
+        }
+      });
+
+  }
+
+  private async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Widgets actualizados',
+      message: 'La información se actualizó correctamente',
+      buttons: [{
+        text: 'Ok',
+        handler: () => {
+          this.router.navigate(['/home/dashboard']);
+        }
+      }]
+    });
+
+    await alert.present();
   }
 }
