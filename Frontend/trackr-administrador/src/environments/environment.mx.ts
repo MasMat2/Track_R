f1dbae6d87bea7bb5acac160bc0a184a9dd94569
Adrian Munoz <amaxmunoz98@gmail.com>
2024-01-05 17:32:26 -0600

    Agregar configuracion atisc.mx
    * urlBackend
    * urlFrontend

---
+++ Frontend/trackr-administrador/src/environments/environment.mx.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  urlBackend: 'https://www.atisc.mx/trackr_dev/api/'
+};
---
+++ Frontend/trackr-paciente/src/environments/environment.mx.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  urlBackend: 'https://www.atisc.mx/trackr_dev/api/',
+  urlFrontend: 'https://www.atisc.mx/trackr_dev/#/',
+};
