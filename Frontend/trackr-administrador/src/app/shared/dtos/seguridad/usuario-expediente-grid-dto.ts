6159d9a65faf61918e7ce0be59e16ec0560bbe86
Johann <johann_antonio85@hotmail.com>
2024-01-17 20:19:05 -0600

    Dtos doctores asociados

--- Frontend/trackr-administrador/src/app/shared/dtos/seguridad/usuario-expediente-grid-dto.ts
+++ Frontend/trackr-administrador/src/app/shared/dtos/seguridad/usuario-expediente-grid-dto.ts
@@ -1,6 +1,7 @@
 export class UsuarioExpedienteGridDTO {
     idExpedienteTrackr: number;
     idUsuario: number;
+    doctorAsociado: string;
     nombreCompleto: string;
     imagenBase64?: string;
     tipoMime?: string;
