d10d0ff79667568f141f4e1820517a7eecb6655a
Erick <118601533+TerickCrue@users.noreply.github.com>
2024-01-24 19:06:58 -0600

    change(mobile): estilos footer

--- Frontend/trackr-paciente/src/app/pages/home/home-routes.ts
+++ Frontend/trackr-paciente/src/app/pages/home/home-routes.ts
@@ -15,7 +15,7 @@ export default [
     children: [
       { path: 'chat', component: ChatPage, data: { breadcrumb: 'Chat' } },
       { path: 'chat-movil', loadChildren: () => import('./chat-movil/chat-movil-routes') },
-      { path: 'muestras', component: MuestrasPage, data: { breadcrumb: 'Muestras' } },
+      { path: 'clinicos', component: MuestrasPage, data: { breadcrumb: 'Muestras' } },
       { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard-routes'), data: { breadcrumb: 'Inicio' }},
       { path: 'config-dashboard', component: ConfiguracionDashboardPage},
       { path: 'cuestionarios',loadChildren: () => import('./cuestionarios/cuestionarios-routes')  ,data: { breadcrumb: 'Cuestionario' } },
--- Frontend/trackr-paciente/src/app/pages/home/layout/footer/footer.component.ts
+++ Frontend/trackr-paciente/src/app/pages/home/layout/footer/footer.component.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { IonTabs } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import {home, videocam, statsChart, documentText, personCircle, chatboxEllipses} from 'ionicons/icons'
-
-
+import {
+  home,
+  videocam,
+  statsChart,
+  statsChartOutline,
+  documentText,
+  documentTextOutline,
+  person,
+  personOutline,
+  chatboxEllipses,
+  chatboxEllipsesOutline
+} from 'ionicons/icons'
 
 @Component({
   selector: 'app-footer',
@@ -17,9 +27,29 @@ import {home, videocam, statsChart, documentText, personCircle, chatboxEllipses}
   ]
 })
 export class FooterComponent implements OnInit {
-  constructor() {
-    addIcons({home, videocam, statsChart, documentText, personCircle, chatboxEllipses})
+
+  selectedTab: string | undefined;
+
+  @ViewChild('tabs') tabs: IonTabs;
+  constructor()
+  {
+    addIcons({
+      home,
+      videocam,
+      statsChart,
+      statsChartOutline,
+      documentText,
+      documentTextOutline,
+      person,
+      personOutline,
+      chatboxEllipses,
+      chatboxEllipsesOutline,
+    })
   }
 
   public ngOnInit(): void {}
+
+  cambiarIconoTabSeleccionada() {
+    this.selectedTab = this.tabs.getSelected();
+  }
 }
