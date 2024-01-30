29867ddd676477ed18b3cf0f31f4514ee7172944
Felix Alanis <felixyahveh@gmail.com>
2024-01-29 21:05:26 -0600

    fix(mobile): mostrar iconos en widgets

--- Frontend/trackr-paciente/src/app/app.component.ts
+++ Frontend/trackr-paciente/src/app/app.component.ts
@@ -11,5 +11,29 @@ import { IonicModule } from '@ionic/angular';
   ]
 })
 export class AppComponent {
-  constructor() {}
+  androidFetchWorkaround() {
+    const originalFetch = (window as any).fetch;
+  
+    (window as any).fetch = (...args:any) => {
+      const [url] = args;
+  
+      if (typeof url === 'string' && url.match(/\.svg/)) {
+        return new Promise((resolve, reject) => {
+          const req = new XMLHttpRequest();
+          req.open('GET', url, true);
+          req.addEventListener('load', () => {
+            resolve({ ok: true, text: () => Promise.resolve(req.responseText) });
+          });
+          req.addEventListener('error', reject);
+          req.send();
+        });
+      } else {
+        return originalFetch(...args);
+      }
+    };
+  }
+  
+  constructor() {
+    this.androidFetchWorkaround();
+  }
 }
