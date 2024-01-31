e2e263c1bb787b4d43cf55891319d566e5800a0d
Felix Alanis <felixyahveh@gmail.com>
2024-01-31 16:47:47 -0600

    add(backend): controladores, servicios y repositorio para la tabla DominioHospital

---
+++ Backend/TrackrAPI/TrackrAPI/Controllers/Catalogo/DominioHospitalController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrackrAPI.Dtos.Catalogo;
+using TrackrAPI.Services.Catalogo;
+
+namespace TrackrAPI.Controllers.Catalogo
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DominioHospitalController : ControllerBase
+    {
+        private readonly DominioHospitalService _dominioHospitalService;
+
+        public DominioHospitalController(DominioHospitalService dominioHospitalService)
+        {
+            _dominioHospitalService = dominioHospitalService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public DominioHospitalDto Consultar(int id)
+        {
+            return _dominioHospitalService.Consultar(id);
+        }
+
+        [HttpPost]
+        public void Agregar(DominioHospitalDto dominioHospitalDto)
+        {
+            _dominioHospitalService.Agregar(dominioHospitalDto);
+        }
+
+        [HttpPut]
+        public void Editar(DominioHospitalDto dominioHospitalDto)
+        {
+            _dominioHospitalService.Modificar(dominioHospitalDto);
+        }
+
+        [HttpDelete]
+        public void Eliminar(DominioHospitalDto dominioHospitalDto)
+        {
+            _dominioHospitalService.Eliminar(dominioHospitalDto);
+        }
+    }
+}
---
+++ Backend/TrackrAPI/TrackrAPI/Repositorys/Catalogo/IDominioHospitalRepository.cs
@@ -0,0 +1,10 @@
+﻿using TrackrAPI.Dtos.Catalogo;
+using TrackrAPI.Models;
+
+namespace TrackrAPI.Repositorys.Catalogo;
+
+public interface IDominioHospitalRepository : IRepository<DominioHospital>
+{
+    public DominioHospitalDto Consultar(int idDominioHospital);
+}
+
