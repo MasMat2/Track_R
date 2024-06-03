0c48c6fa6879dadb881386c0fb4b2fb6211044fa
LuisDiazOSXminiCDIS <99diazluisfernand@gmail.com>
2024-06-03 16:54:11 -0600

    change title app for play store

--- Frontend/trackr-paciente/capacitor.config.ts
+++ Frontend/trackr-paciente/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'trackr.app',
-  appName: 'trackr-paciente',
+  appName: 'Oncotracker',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
