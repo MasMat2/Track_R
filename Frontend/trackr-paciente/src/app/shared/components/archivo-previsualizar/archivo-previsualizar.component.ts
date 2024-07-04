14599b04ce079f3f5d2595a859cada13aacd0bfd
Erick <118601533+TerickCrue@users.noreply.github.com>
2024-07-04 05:17:08 -0600

    change(mobile): previsualizador de archivo
    Estandarizado el componente de previsualizar archivo.
    Ajuste de estilo y funcionalidad.

--- Frontend/trackr-paciente/src/app/shared/components/archivo-previsualizar/archivo-previsualizar.component.ts
+++ Frontend/trackr-paciente/src/app/shared/components/archivo-previsualizar/archivo-previsualizar.component.ts
@@ -10,6 +10,7 @@ import { addIcons } from 'ionicons';
 import { ArchivoService } from '@http/archivo/archivo.service';
 import { BehaviorSubject } from 'rxjs';
 import { AlertController } from '@ionic/angular/standalone';
+import { url } from 'inspector';
 
 
 
@@ -21,11 +22,19 @@ import { AlertController } from '@ionic/angular/standalone';
   imports: [CommonModule, IonicModule, NgxExtendedPdfViewerModule],
 })
 export class ArchivoPrevisualizarComponent implements OnInit, AfterViewInit {
-  protected tipo: string;
+  
+  protected fileSource: 'id' | 'url';
+  protected urlArchivo: string;
   protected idArchivo: number;
+
   protected archivo: ArchivoGetDTO;
-  protected tipoMime: string;
   protected archivoBase64: string;
+
+  protected tipo: string;
+  protected tipoMime: string;
+
+  protected mostrarTitulo: boolean = false;
+  protected titulo: string;
 
   //Estado de "cargando" para mostrar el alert con spinner
   private cargandoSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
@@ -54,23 +63,28 @@ export class ArchivoPrevisualizarComponent implements OnInit, AfterViewInit {
     });
 
     this.cargandoSubject.next(true);
-    this.archivoService.getArchivo(this.idArchivo).subscribe({
-      next: (res)=> {
-        this.archivo = res;
-        this.tipoMime = this.archivo.archivoMime.split('/')[0];
-        this.archivoBase64 = 'data:' + this.archivo.archivoMime + ';base64,' + this.archivo.archivo;
-      },
-      error: ()=> {},
-      complete: () => {
-        this.cargandoSubject.next(false);
+
+    switch(this.fileSource){
+      case 'id' : {
+        this.descargarArchivoById(this.idArchivo);
+        break;
       }
-    })
+      case 'url' : {
+        this.descargarArchivoByUrl(this.urlArchivo);
+        break;
+      }
+      default: {
+        console.error("TIPO NO ESPECIFICADO");
+        this.presentAlertError();
+      }
+      
+    }
   }
 
   ngAfterViewInit(): void {
   }
 
-  async presentLoading() {
+  private async presentLoading() {
     this.loading = await this.alertController.create({
       cssClass: "custom-alert-loading",
       backdropDismiss: false,
@@ -78,21 +92,15 @@ export class ArchivoPrevisualizarComponent implements OnInit, AfterViewInit {
     return await this.loading.present();
   }
 
-  async dismissLoading() {
+  private async dismissLoading() {
     if (this.loading) {
       await this.loading.dismiss();
       this.loading = null;
     }
   }
 
-
-  regresarBtn() {
-    this.modalController.dismiss();
-  }
-
-  descargarArchivo() {
+  protected descargarArchivo() {
     if (this.plataformaService.isMobile()) {
-
       this.downloadFileMobile(this.archivo.archivo, this.archivo.nombre, this.archivo.archivoMime)
     }
     else if (this.plataformaService.isWeb()) {
@@ -100,7 +108,7 @@ export class ArchivoPrevisualizarComponent implements OnInit, AfterViewInit {
     }
   }
 
-  async downloadFileMobile(fileBase64: string, nombre?: string, mime?: string) {
+  private async downloadFileMobile(fileBase64: string, nombre?: string, mime?: string) {
     try {
 
       let downloadDirectory = Directory.Documents
@@ -115,16 +123,15 @@ export class ArchivoPrevisualizarComponent implements OnInit, AfterViewInit {
       });
 
       // Obtener la URL del archivo creado
-      const url = result.uri;
-
-      console.log(url)
+      //const url = result.uri;
+      //console.log(url)
 
     } catch (error) {
       console.error('Error al descargar el archivo:', error);
     }
   }
 
-  downloadFileWeb(fileBase64: string, nombre?: string, mime?: string) {
+  private downloadFileWeb(fileBase64: string, nombre?: string, mime?: string) {
     // Decodificar la cadena Base64
     const decodedData = atob(fileBase64);
 
@@ -152,6 +159,67 @@ export class ArchivoPrevisualizarComponent implements OnInit, AfterViewInit {
 
     // Limpiar el object URL después de la descarga
     URL.revokeObjectURL(url);
+  }
+
+  private descargarArchivoById(idArchivo: number){
+    if(idArchivo){
+      this.archivoService.getArchivo(idArchivo).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.archivo = data;
+          this.tipoMime = this.archivo.archivoMime.split('/')[0];
+          this.archivoBase64 = 'data:' + this.archivo.archivoMime + ';base64,' + this.archivo.archivo;
+        },
+        error: () => {
+          this.cargandoSubject.next(false);
+          this.presentAlertError();
+        },
+        complete: () => {
+          this.cargandoSubject.next(false);
+        }
+      })
+    }
+  }
+
+  private descargarArchivoByUrl(urlArchivo: string){
+    if(urlArchivo){
+      this.archivoService.getArchivoByUrl(urlArchivo).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.archivo = data;
+          this.tipoMime = this.archivo.archivoMime.split('/')[0];
+          this.archivoBase64 = 'data:' + this.archivo.archivoMime + ';base64,' + this.archivo.archivo;
+        },
+        error: () => {
+          this.cargandoSubject.next(false);
+          this.presentAlertError();
+        },
+        complete: () => {
+          this.cargandoSubject.next(false);
+        }
+      })
+    }
+  }
+
+  protected regresarBtn() {
+    this.modalController.dismiss();
+  }
+
+  protected async presentAlertError(){
+    const alert = await this.alertController.create({
+      header: 'Error',
+      subHeader: 'Ha ocurrido un error al mostrar este archivo',
+      cssClass: 'custom-alert color-error icon-trash',
+      buttons: [{
+        text: 'Ok',
+        role: 'confirm',
+        handler: () => {
+          this.regresarBtn();
+        }
+      }]
+    })
+
+    alert.present();
   }
 
 }
