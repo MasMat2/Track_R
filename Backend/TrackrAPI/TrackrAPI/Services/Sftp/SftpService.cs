13857a6da322b89e0dd0d1906d0d5894c3d4a4ee
Erick <118601533+TerickCrue@users.noreply.github.com>
2024-07-19 14:49:08 -0600

    fix(backend): sftpService
    Exception cuando el archivo no existe.

--- Backend/TrackrAPI/TrackrAPI/Services/Sftp/SftpService.cs
+++ Backend/TrackrAPI/TrackrAPI/Services/Sftp/SftpService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Hosting.Internal;
 using Renci.SshNet;
+using Renci.SshNet.Common;
 using System.IO;
 using TrackrAPI.Dtos.Seguridad;
 using TrackrAPI.Repositorys.Sftp;
@@ -159,13 +160,27 @@ namespace TrackrAPI.Services.Sftp
                         // Download File
                         using (Stream fileStream = new FileStream(localFilePath, FileMode.OpenOrCreate))
                         {
-                            sftp.DownloadFile(linuxPath, fileStream);
+                            try
+                            {
+                                sftp.DownloadFile(linuxPath, fileStream);
+                            }
+                            catch (SftpPathNotFoundException ex)
+                            {
+                                Console.WriteLine("The file does not exist.");
+                            }
                         }
-                        
+
                         // Update Cache
-                        var fileInfo = sftp.GetAttributes(linuxPath);
-                        this.UpdateCache(filePath, fileInfo.LastWriteTime);
-                        
+                        try
+                        {
+                            var fileInfo = sftp.GetAttributes(linuxPath);
+                            this.UpdateCache(filePath, fileInfo.LastWriteTime);
+                        }
+                        catch(SftpPathNotFoundException ex)
+                        {
+                            Console.WriteLine("The file does not exist.");
+                        }
+
                         sftp.Disconnect();
                     }
                 }
