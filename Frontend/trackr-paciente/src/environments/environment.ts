fa7724992609054d0150d933737012128e1c8f7a
LuisDiazOSXminiCDIS <99diazluisfernand@gmail.com>
2024-08-06 10:03:41 -0600

    Build ios 84
    Servidor ios: azure
    Grupo de pruebas: US-IT

--- Frontend/trackr-paciente/src/environments/environment.ts
+++ Frontend/trackr-paciente/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  //urlBackend: 'https://oncotracker-dev.azurewebsites.net/api',
-  //urlFrontend: 'https://oncotracker-dev.azurewebsites.net/#/',
-  urlBackend: 'https://www.atisc.mx/trackr_dev/api/',
-  urlFrontend: 'https://www.atisc.mx/trackr_dev/#/',
+  urlBackend: 'https://oncotracker-dev.azurewebsites.net/api',
+  urlFrontend: 'https://oncotracker-dev.azurewebsites.net/#/',
+  //urlBackend: 'https://www.atisc.mx/trackr_dev/api/',
+  //urlFrontend: 'https://www.atisc.mx/trackr_dev/#/',
 
 };
